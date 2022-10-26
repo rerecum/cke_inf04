98eb64899c472d2cf706b67005c21e730fb71943
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-10-26 10:14:02 +0200

    button working

--- app/src/main/java/com/example/cke_inf04/MainActivity.kt
+++ app/src/main/java/com/example/cke_inf04/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.cke_inf04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.confirm).setOnClickListener {
+            val myString=findViewById<EditText>(R.id.mail).text
+            for (i in 0..myString.length-1) {
 
+                if (myString.substring(i,i+1) == "@"){
+                    findViewById<Button>(R.id.confirm).text = "jest @"
+                    break
+                }
+                else{
+
+                    findViewById<Button>(R.id.confirm).text = "nie ma @"
+                }
+            }
+            //findViewById<Button>(R.id.confirm).text=myString.substring(1,2)
+        //if (R.id.mail == )
+        }
     }
 }
